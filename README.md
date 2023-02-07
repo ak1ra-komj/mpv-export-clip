b79a97581a17310f3ae12b1967f7cf5ea7e329bc
ak1ra <git@ak1ra.xyz>
2023-02-07 19:52:41 +0800

    file reformat

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ a mpv lua script to cut clips from video
 
 ```sh
 mkdir -p ~/.config/mpv/scripts
-wget https://raw.githubusercontent.com/WindProphet/mpv-export-clip/master/export_clip.lua -O ~/.config/mpv/scripts/export_clip.lua
+wget https://raw.githubusercontent.com/citrus-lemon/mpv-export-clip/master/export_clip.lua -O ~/.config/mpv/scripts/export_clip.lua
 ```
 
 and register function
@@ -14,14 +14,16 @@ and register function
 add this to `~/.config/mpv/input.conf`
 
 ```
-Ctrl+2 script-message export-loop-clip
-a script-message seek-ab-loop-a
-b script-message seek-ab-loop-b
-Ctrl+a script-message set-ab-loop-a
-Ctrl+b script-message set-ab-loop-b
+# mpv-export-clip/export_clip.lua
+a          script-message set-ab-loop-a
+b          script-message set-ab-loop-b
+Ctrl+a     script-message seek-ab-loop-a
+Ctrl+b     script-message seek-ab-loop-b
+e          script-message export-loop-clip
+
 # add following for moving pointer faster
-Alt+RIGHT frame-step
-Alt+LEFT frame-back-step
+Alt+RIGHT  frame-step
+Alt+LEFT   frame-back-step
 ```
 
 clip will be saved in `screenshot-directory` folder
--- export_clip.lua
+++ export_clip.lua
@@ -2,102 +2,102 @@ utils = require "mp.utils"
 
 -- test available filename
 function slow_start()
-  local basename = mp.get_property("filename"):gsub("%.([^%.]+)$", "")
-  local screenshot_folder = mp.get_property("screenshot-directory") or ""
+    local basename = mp.get_property("filename"):gsub("%.([^%.]+)$", "")
+    local screenshot_folder = mp.get_property("screenshot-directory") or ""
 
-  function filename(idx)
-    return utils.join_path(
-      screenshot_folder,
-      basename .. '-' .. string.format("%04d",idx) .. '.mp4'
-    )
-  end
-
-  -- multiply
-  local inc = 1
-  while utils.file_info(filename(inc)) do
-    inc = inc * 2
-  end
-
-  -- narrowing
-  local lower_bound = math.floor(inc / 2)
-  while inc - lower_bound > 1 do
-    local mid = math.floor((inc + lower_bound) / 2)
-    local is_file = utils.file_info(filename(mid))
-    if is_file then
-      lower_bound = mid
-    else
-      inc = mid
+    function filename(idx)
+        return utils.join_path(screenshot_folder, basename .. '-' .. string.format("%04d", idx) .. '.mp4')
     end
-  end
 
-  return filename(inc)
+    -- multiply
+    local inc = 1
+    while utils.file_info(filename(inc)) do
+        inc = inc * 2
+    end
+
+    -- narrowing
+    local lower_bound = math.floor(inc / 2)
+    while inc - lower_bound > 1 do
+        local mid = math.floor((inc + lower_bound) / 2)
+        local is_file = utils.file_info(filename(mid))
+        if is_file then
+            lower_bound = mid
+        else
+            inc = mid
+        end
+    end
+
+    return filename(inc)
 end
 
 function export_loop_clip()
-  local a = mp.get_property_number("ab-loop-a")
-  local b = mp.get_property_number("ab-loop-b")
-  local path = mp.get_property("path")
-  if a and b then
-    local outfile = slow_start()
-    local cmd = {
-      "run",
-      "ffmpeg",
-      "-ss", tostring(a),
-      "-i", path,
-      "-t", tostring(b-a),
-      "-c:v", "libx264",
-      "-crf", "21",
-      "-preset:v", "placebo",
-      "-pix_fmt", "yuva420p",
-      "-filter_complex", "scale=iw*min(1\\,min(1280/iw\\,720/ih)):-2",
-      "-an", "-sn",
-      "-map_metadata", "-1",
-      "-v", "error",
-      "-n",
-      outfile
-    }
-    local args = { args = cmd }
-    function cb(success, result, error)
-      if success then
-        mp.msg.info("save clip " .. outfile)
-        mp.osd_message("save clip " .. outfile)
-      else
-        mp.msg.info(error)
-        mp.osd_message("export loop clip error: " .. error)
-      end
+    local a = mp.get_property_number("ab-loop-a")
+    local b = mp.get_property_number("ab-loop-b")
+    local path = mp.get_property("path")
+    if a and b then
+        local outfile = slow_start()
+        local cmd = {
+            "run",
+            "ffmpeg",
+            "-ss", tostring(a),
+            "-i", path,
+            "-t", tostring(b-a),
+            "-c:v", "libx264",
+            -- "-b:v", "3000k",
+            "-crf", "21",
+            "-preset:v", "placebo",
+            "-pix_fmt", "yuva420p",
+            "-filter_complex", "scale=iw*min(1\\,min(1280/iw\\,720/ih)):-2",
+            "-an", "-sn",
+            "-map_metadata", "-1",
+            "-v", "error",
+            "-n",
+            outfile
+        }
+        local args = {
+            args = cmd
+        }
+        function cb(success, result, error)
+            if success then
+                mp.msg.info("save clip " .. outfile)
+                mp.osd_message("save clip " .. outfile)
+            else
+                mp.msg.info(error)
+                mp.osd_message("export loop clip error: " .. error)
+            end
+        end
+        mp.command_native_async(cmd, cb)
     end
-    mp.command_native_async(cmd, cb)
-  end
 end
 mp.register_script_message("export-loop-clip", export_loop_clip)
 
 function set_ab_loop_a()
-  local pos = mp.get_property_number("time-pos")
-  local a = mp.get_property_number("ab-loop-a")
-  mp.set_property_number("ab-loop-a", pos)
-  mp.osd_message('set A-B loop A: ' .. tostring(pos))
+    local pos = mp.get_property_number("time-pos")
+    local a = mp.get_property_number("ab-loop-a")
+    mp.set_property_number("ab-loop-a", pos)
+    mp.osd_message('set A-B loop A: ' .. tostring(pos))
 end
 mp.register_script_message("set-ab-loop-a", set_ab_loop_a)
 
 function set_ab_loop_b()
-  local pos = mp.get_property_number("time-pos")
-  mp.set_property_number("ab-loop-b", pos)
-  mp.osd_message('set A-B loop B: ' .. tostring(pos))
+    local pos = mp.get_property_number("time-pos")
+    mp.set_property_number("ab-loop-b", pos)
+    mp.osd_message('set A-B loop B: ' .. tostring(pos))
 end
 mp.register_script_message("set-ab-loop-b", set_ab_loop_b)
 
 function seek_ab_loop_a()
-  local pos = mp.get_property_number("ab-loop-a")
-  if pos then
-    mp.set_property_number("time-pos", pos)
-  end
+    local pos = mp.get_property_number("ab-loop-a")
+    if pos then
+        mp.set_property_number("time-pos", pos)
+    end
 end
 mp.register_script_message("seek-ab-loop-a", seek_ab_loop_a)
 
 function seek_ab_loop_b()
-  local pos = mp.get_property_number("ab-loop-b")
-  if pos then
-    mp.set_property_number("time-pos", pos)
-  end
+    local pos = mp.get_property_number("ab-loop-b")
+    if pos then
+        mp.set_property_number("time-pos", pos)
+    end
 end
 mp.register_script_message("seek-ab-loop-b", seek_ab_loop_b)
